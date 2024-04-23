430dda844a0fdc2f2c9e6be9396b8e41a2883e51
Roman Chaban <roman.chaban1001@gmail.com>
2024-04-23 19:17:39 +0300

    Modified App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Router } from './common/Router/Router';
 import { RouterProvider } from 'react-router-dom';
+import './assets/styles/main.module.scss';
 
 export const App: FC = () => {
   return <RouterProvider router={Router} />;
