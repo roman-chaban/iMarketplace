85be42ca4bf60d73b2c7d28c55bf3700351309a9
Roman Chaban <roman.chaban1001@gmail.com>
2024-04-23 20:06:58 +0300

    App styles fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Router } from './common/Router/Router';
 import { RouterProvider } from 'react-router-dom';
-import './assets/styles/main.module.scss';
+import './assets/styles/main.scss';
 
 export const App: FC = () => {
   return <RouterProvider router={Router} />;
