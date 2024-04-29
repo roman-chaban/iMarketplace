03a9d95e663826926dcf9d9f96752aa88d27db05
Roman Chaban <roman.chaban1001@gmail.com>
2024-04-29 19:38:28 +0300

    Fixed Header/Hero components

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,25 +1,14 @@
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 import styles from './hero.module.scss';
 import { Slider } from '../Slider/Slider';
 import AppleLogo from '../../favicon/favicon.png';
 import { HeroSection, MainTitle } from './styled/hero';
 
-type IsShowTitle = boolean;
-
 export const Hero: FC = (): JSX.Element => {
-  const [isShowTitle, setIsShowTitle] = useState<IsShowTitle>(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsShowTitle(true);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <HeroSection>
       <div className={styles.hero__container}>
-        {isShowTitle && (
+        {
           <MainTitle className={styles.mainHero__title}>
             Welcome to iMarketplace! It's great to have you here.
             <img
@@ -30,7 +19,7 @@ export const Hero: FC = (): JSX.Element => {
               style={{ marginLeft: '40px' }}
             />
           </MainTitle>
-        )}
+        }
         <div className={styles.swiper__div}>
           <Slider slides={[]} />
         </div>
