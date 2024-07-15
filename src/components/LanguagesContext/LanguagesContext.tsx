e8aa4c39ab392f298738a88a3b1bda9ae577a638
Roman Chaban <roman.chaban1001@gmail.com>
2024-07-14 21:08:07 -0800

    Modified LanguagesContext.tsx file

--- src/components/LanguagesContext/LanguagesContext.tsx
+++ src/components/LanguagesContext/LanguagesContext.tsx
@@ -1,35 +1,42 @@
-import { ReactNode, createContext, useState, useEffect } from 'react';
+import { ReactNode, createContext, useState, useEffect } from "react";
 
 interface LanguageProviderProps {
   children: ReactNode;
 }
 
 interface LanguageContextProps {
-  translations: any;
+  translations: { [key: string]: string };
   currentLanguage: string;
   handleChangeLanguage: (language: string) => void;
 }
 
 export const LanguageContext = createContext<LanguageContextProps>({
-  currentLanguage: 'en',
+  currentLanguage: "en",
   handleChangeLanguage: () => {},
+  translations: {},
 });
 
 export const LanguageProvider = ({ children }: LanguageProviderProps) => {
   const [currentLanguage, setCurrentLanguage] = useState<string>(
-    localStorage.getItem('selectedLanguage') || 'en'
+    localStorage.getItem("selectedLanguage") || "en"
   );
+
+  const translations = {
+    en: "English",
+  };
 
   const handleChangeLanguage = (language: string) => {
     setCurrentLanguage(language);
   };
 
   useEffect(() => {
-    localStorage.setItem('selectedLanguage', currentLanguage);
+    localStorage.setItem("selectedLanguage", currentLanguage);
   }, [currentLanguage]);
 
   return (
-    <LanguageContext.Provider value={{ currentLanguage, handleChangeLanguage }}>
+    <LanguageContext.Provider
+      value={{ translations, currentLanguage, handleChangeLanguage }}
+    >
       {children}
     </LanguageContext.Provider>
   );
